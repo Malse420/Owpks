11b723b8f100427c3e33d1f71d7ffaeb0fa28bba
Malse420 <100170927+Malse420@users.noreply.github.com>
2024-10-20 17:58:17 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Start Tailscale
-tailscaled &
-tailscale up --authkey=${TAILSCALE_AUTH_KEY}
+# Switch back to root for running Tailscale, which requires elevated privileges
+sudo tailscaled &
+sudo tailscale up --authkey=${TAILSCALE_AUTH_KEY}
 
 # Start Stable Diffusion WebUI (1111)
 bash webui.sh --listen --port 7860
